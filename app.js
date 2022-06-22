3894497aa18f1b11d6507cacb6594d4267cf0b17
Robert Adewunmi <robertadewunmi@Roberts-MBP.localdomain>
2022-06-22 11:27:12 +0100

    edit text

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello world!"));
+app.get("/", (req, res) => res.send("Hello HICC world!"));
 
 app.get("/health", (req, res) => {
   res.status(200);
